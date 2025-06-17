b332ba6a76a49f2bd3117617d29d897dd088aafc
Jonathan Martinez <pamartinez@tigo.com.gt>
2025-06-16 18:03:49 -0600

    added jwt validation

--- backend/src/main/java/com/appsports/backend/config/SecurityConfig.java
+++ backend/src/main/java/com/appsports/backend/config/SecurityConfig.java
@@ -1,28 +1,72 @@
 package com.appsports.backend.config;
 
+import com.appsports.backend.model.entities.Token;
+import com.appsports.backend.repository.TokenRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpHeaders;
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 @EnableWebSecurity
 @RequiredArgsConstructor
 @EnableMethodSecurity
 public class SecurityConfig {
+
+    private final JwtAuthFilter jwtAuthFilter;
+    private final AuthenticationProvider authenticationProvider;
+    private final TokenRepository tokenRepository;
+
     @Bean
-    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+    public SecurityFilterChain securityFilterChain(HttpSecurity http, AuthenticationProvider authenticationProvider) throws Exception {
         http
-                .csrf(csrf -> csrf.disable())
-                .authorizeHttpRequests(auth -> auth.anyRequest().permitAll());
+                .csrf(AbstractHttpConfigurer::disable)
+                .authorizeHttpRequests( req ->
+                        req.requestMatchers("/auth/**")
+                                .permitAll()
+                                .anyRequest()
+                                .authenticated()
+                )
+                .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .authenticationProvider(authenticationProvider)
+                .addFilterBefore(jwtAuthFilter, UsernamePasswordAuthenticationFilter.class)
+                .logout(logout ->
+                        logout.logoutUrl("/auth/logout")
+                                .addLogoutHandler((request, response, authentication) -> {
+                                    final var authHeader = request.getHeader(HttpHeaders.AUTHORIZATION);
+                                    logout(authHeader);
+                                })
+                                .logoutSuccessHandler((request, response, authentication) ->
+                                        SecurityContextHolder.clearContext())
+                );
+
+        ;
 
         return http.build();
+    }
+
+    private void logout(final String token) {
+        if (token == null || !token.startsWith("Bearer ")) {
+            throw new IllegalArgumentException("Invalid JWT token");
+        }
+
+        final String jwtToken = token.substring(7);
+        final Token foundToken = tokenRepository.findByToken(jwtToken)
+                .orElseThrow(() -> new IllegalArgumentException("Invalid JWT token"));
+        foundToken.setExpired(true);
+        foundToken.setRevoked(true);
+        tokenRepository.save(foundToken);
     }
 
     @Bean
--- backend/src/main/java/com/appsports/backend/repository/TokenRepository.java
+++ backend/src/main/java/com/appsports/backend/repository/TokenRepository.java
@@ -5,6 +5,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface TokenRepository extends JpaRepository<Token, Integer> {
     @Query(value = """
@@ -13,4 +14,6 @@ public interface TokenRepository extends JpaRepository<Token, Integer> {
       where u.id = :id and (t.expired = false or t.revoked = false)\s
       """)
     List<Token> findAllValidTokenByUser(Integer id);
+
+    Optional<Token> findByToken(String token);
 }
